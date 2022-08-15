3b18e107de8fd4a9f56183fde668a4a574697f16
SERG <testsproba8@gmail.com>
2022-08-15 11:24:51 +0300

    changed the name of function to test_find_add_to_basket_btn

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 import time
 
-def test_find_add_to_card_btn(browser):
+def test_find_add_to_basket_btn(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     time.sleep(15)
     assert browser.find_element(By.CLASS_NAME, 'btn-add-to-basket').is_displayed(), \
